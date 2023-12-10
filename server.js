5d7afcd024aab494e1c1071da437bd4a79e9aee5
NikhitaBoyidapu <nboyidap@uncc.edu>
2023-12-10 17:08:10 -0500

    Backend code

---
+++ server.js
@@ -0,0 +1,184 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const expressJwt = require('express-jwt'); 
+const jwt = require('jsonwebtoken');
+const compression = require('compression');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+const secretKey = 'My super secret key';
+const jwtMW = expressJwt({
+  secret: secretKey,
+  algorithms: ['HS256']
+});
+
+var connection = mysql.createConnection({
+  host: 'sql5.freemysqlhosting.net',
+  user: 'sql5667961',
+  password: 'iW3RJILcF1',
+  database: 'sql5667961'
+});
+
+// async function passwordEncrption(password) {
+//     const saltRounds = 10;
+//     const hashedPassword = await bcrypt.hash(password, saltRounds);
+//     return hashedPassword;
+// }
+function  changeDateFormat(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+app.get('/api/budget/:username', (req, res) => {
+  const username = req.params.username;
+
+  // Assuming there's a table named 'budgets' with a structure that fits your needs
+  const sql = 'SELECT * FROM Budget WHERE username = ?';
+
+  connection.query(sql, [username], (error, results) => {
+    if (error) {
+      console.error('Error querying the database:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post('/api/signup', async (req, res) => {
+  try {
+    const { username, name, email, password } = req.body;
+    console.log('req.body::', req.body);
+    console.log('username', username);
+    console.log('password', password);
+    console.log('name', name);
+    console.log('email', email);
+
+    // const pwd = await passwordEncryption(password);
+    const date = changeDateFormat(new Date());
+    
+    connection.connect();
+    
+    connection.query('INSERT INTO User VALUES (?,?,?,?,?)', [username, email, password, date, name], function (error, results, fields) {
+
+      if (error) {
+        console.error('Error inserting user:', error);
+        res.status(500).json({
+          success: false,
+          error: 'Internal Server Error'
+        });
+        return;
+      }
+
+      res.json({
+        success: true
+      });
+    });
+  } catch (err) {
+    console.error('Unexpected error during signup:', err);
+    res.status(500).json({
+      success: false,
+      error: 'Internal Server Error'
+    });
+  }
+});
+
+
+app.post('/addbudget', async (req, res) => {
+  try {
+    const {
+      title,
+      budget,
+      color,
+      expense,
+      username,
+      month
+    } = req.body;
+    
+    console.log('req.body::', req.body);
+    console.log('title', title);
+    console.log('budget', budget);
+    console.log('color', color);
+    console.log('expense', expense);
+    console.log('username', username);
+
+    connection.connect();
+
+    const sql = `
+      INSERT INTO Budget (title, budget, color, expense, username, month)
+      VALUES (?, ?, ?, ?, ?, ?)
+      ON DUPLICATE KEY UPDATE
+        budget = VALUES(budget),
+        color = VALUES(color),
+        expense = VALUES(expense)
+    `;
+
+    connection.query(sql, [title.toUpperCase(), budget, color, expense, username, month], function (error, results, fields) {
+
+      if (error) {
+        console.error('Error inserting or updating budget:', error);
+        res.status(500).json({
+          success: false,
+          error: 'Internal Server Error'
+        });
+        return;
+      }
+
+      res.json({
+        success: true
+      });
+    });
+  } catch (err) {
+    console.error('Unexpected error during budget operation:', err);
+    res.status(500).json({
+      success: false,
+      error: 'Internal Server Error'
+    });
+  }
+});
+
+
+app.post('/api/login', (req, res) => {
+  const sql = 'SELECT * FROM User WHERE Email=? AND Password=?';
+  const values = [req.body.email, req.body.password];
+
+  connection.query(sql, values, (error, results) => {
+    if (error) throw error;
+
+    const user = results[0]; // Assuming there is only one user with the given credentials
+
+    if (user) {
+      // Create a token with an expiration time of 1 minute
+      const token = jwt.sign({ email: user.Email }, secretKey, { expiresIn: '1m' });
+
+      res.json({
+        success: true,  
+        token: token,
+        username: user.Username, 
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        token: null,
+        err: 'Email or Password is incorrect',
+      });
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`API served at http://localhost:${port}`);
+});
+
